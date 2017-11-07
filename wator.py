8a87397f0664d35b7742009f875f1abb1ef9766e
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2017-11-07 08:40:18 +0100

    Fix moving bugs

--- wator.py
+++ wator.py
@@ -9,7 +9,7 @@ class WaTor:
                  energy_initial=None, energies=None, energy_eat=None):
         """Setup WaTor simulation."""
         self.age_fish = age_fish if age_fish else 5
-        self.age_shark = age_shark if age_shark else 10
+        self.age_shark = -age_shark if age_shark else -10
         self.energy_initial = energy_initial if energy_initial else 5
         self.energy_eat = energy_eat if energy_eat else 3
 
@@ -32,7 +32,7 @@ class WaTor:
             fish_index = random_index[:nfish]
             self.creatures.flat[fish_index] = fish_ages
 
-            shark_ages = numpy.random.randint(-self.age_shark, 0, size=nsharks)
+            shark_ages = numpy.random.randint(self.age_shark, 0, size=nsharks)
             shark_index = random_index[nfish:]
             self.creatures.flat[shark_index] = shark_ages
 
@@ -45,7 +45,7 @@ class WaTor:
             self.energies = numpy.zeros_like(self.creatures, dtype=numpy.int)
             self.energies[self.creatures < 0] = self.energy_initial
 
-        self.width, self.height = self.creatures.shape
+        self.height, self.width = self.creatures.shape
 
     def count_fish(self):
         """Return number of fish."""
@@ -58,62 +58,80 @@ class WaTor:
         return numpy.sum(self.creatures < 0)
 
     def generate_move(self, x, y):
-        directions = [[-1, -1], [-1, 1], [1, -1], [1, 1]]
+        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
         for i, j in numpy.random.permutation(directions):
-            yield (x + i) % self.width, (y + j) % self.height
+            a, b = (x + i) % self.height, (y + j) % self.width
+            if (a, b) != (x, y):  # cannot move to same field
+                yield a, b
 
     def move_fish(self):
         new_creatures = numpy.copy(self.creatures)
-        new_creatures[new_creatures > 0] = 0
 
         for x, y in numpy.argwhere(self.creatures > 0):  # for each fish
             moved = False
-
             for a, b in self.generate_move(x, y):
-                if new_creatures[a, b] == 0:  # move if the field is empty
-                    new_creatures[a, b] = self.creatures[x, y] + 1
+                if new_creatures[a, b] == 0:
+                    if new_creatures[x, y] + 1 > self.age_fish:
+                        # reproduce
+                        new_creatures[x, y] = 1
+                        new_creatures[a, b] = 1
+                    else:
+                        new_creatures[a, b] = self.creatures[x, y] + 1
+                        new_creatures[x, y] = 0
                     moved = True
+                    break
 
             if not moved:
-                new_creatures[x, y] = self.creatures[x, y] + 1
+                new_creatures[x, y] = min(self.creatures[x, y] + 1,
+                                          self.age_fish)
 
         return new_creatures
 
     def move_sharks(self):
         new_creatures = numpy.copy(self.creatures)
-        new_creatures[new_creatures < 0] = 0
         new_energies = numpy.copy(self.energies)
 
         for x, y in numpy.argwhere(self.creatures < 0):  # for each shark
             moved = False
-
             for a, b in self.generate_move(x, y):
                 if new_creatures[a, b] > 0:  # if there is fish eat it
+                    # TODO reproduce
                     new_creatures[a, b] = self.creatures[x, y] - 1
+                    new_creatures[x, y] = 0
                     new_energies[a, b] = self.energies[x, y] + self.energy_eat
+                    new_energies[x, y] = 0
                     moved = True
+                    break
 
             if moved:
                 continue
 
             for a, b in self.generate_move(x, y):
-                if new_creatures[a, b] == 0:  # if there is fish eat it
+                if new_creatures[a, b] == 0:
+                    # TODO reproduce
+                    #if new_creatures[a, b] < self.age_shark:
+                    #    # reproduce
+                    #    new_creatures[x, y], new_creatures[a, b] = -1, -1
+                    #    new_energies[a, b] = new_energies[x, y]
                     new_creatures[a, b] = self.creatures[x, y] - 1
+                    new_creatures[x, y] = 0
                     new_energies[a, b] = self.energies[x, y]
+                    new_energies[x, y] = 0
                     moved = True
+                    break
 
             if not moved:
-                new_creatures[x, y] = self.creatures[x, y] + 1
+                new_creatures[x, y] = max(self.creatures[x, y] - 1,
+                                          self.age_shark)
                 new_energies[x, y] = self.energies[x, y]
 
         new_creatures[new_energies == 1] = 0
         new_energies[new_energies > 0] -= 1
-        self.energies = new_energies
 
-        return new_creatures
+        return new_creatures, new_energies
 
     def tick(self):
         """Simulate one chronon."""
         self.creatures = self.move_fish()
-        self.creatures = self.move_sharks()
+        self.creatures, self.energies = self.move_sharks()
         return self
