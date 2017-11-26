713ce2abe88ecb9b5f6524dac34c205aacf71181
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2017-11-26 20:02:44 +0100

    Provide some optimalizations

--- wator.pyx
+++ wator.pyx
@@ -1,84 +1,83 @@
 import numpy
 cimport numpy
+cimport cython
 
 
 cdef class WaTor:
     cdef int age_fish, age_shark
-    cdef int energy_initial, energy_eat
+    cdef numpy.int16_t energy_initial, energy_eat
     cdef int width, height
     cdef public numpy.ndarray creatures
     cdef public numpy.ndarray energies
 
-    def _random_creatures(self, shape, nfish, nsharks):
-        creatures = numpy.zeros(shape, dtype=numpy.int8)
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    cdef numpy.ndarray[numpy.int8_t, ndim=2] _random_creatures(self, int height, int width, int nfish, int nsharks):
+        cdef numpy.ndarray[numpy.int8_t, ndim=2] creatures = numpy.zeros((height, width), dtype=numpy.int8)
+        cdef numpy.ndarray[numpy.int32_t] random_index = numpy.random.choice(numpy.arange(height * width, dtype=numpy.int32), size=nfish + nsharks, replace=False)
 
-        wator_size = shape[0] * shape[1]
-        wator_index = numpy.arange(wator_size)
-        random_index = numpy.random.choice(wator_index, size=nfish + nsharks,
-                                           replace=False)
-
-        fish_ages = numpy.random.randint(1, self.age_fish + 1, size=nfish)
-        fish_index = random_index[:nfish]
-        creatures.flat[fish_index] = fish_ages
-
-        shark_ages = numpy.random.randint(self.age_shark, 0, size=nsharks)
-        shark_index = random_index[nfish:]
-        creatures.flat[shark_index] = shark_ages
+        creatures.flat[random_index[:nfish]] = numpy.random.randint(1, self.age_fish + 1, size=nfish, dtype=numpy.int8)
+        creatures.flat[random_index[nfish:]] = numpy.random.randint(self.age_shark, 0, size=nsharks, dtype=numpy.int8)
 
         return creatures
 
-    def __cinit__(self, creatures=None, shape=None, nfish=None, nsharks=None,
-            age_fish=None, age_shark=None, energy_initial=None, energies=None,
-            energy_eat=None):
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    def __cinit__(self, numpy.ndarray creatures=None, shape=None, int nfish=-1, int nsharks=-1,
+            int age_fish=-1, int age_shark=-1, int energy_initial=-1, numpy.ndarray energies=None, int energy_eat=-1):
         """Setup WaTor simulation."""
         # member variables setup
-        self.age_fish = age_fish if age_fish else 5
-        self.age_shark = -age_shark if age_shark else -10
-        self.energy_initial = energy_initial if energy_initial else 5
-        self.energy_eat = energy_eat if energy_eat else 3
+        self.age_fish = age_fish if age_fish != -1 else 5
+        self.age_shark = -age_shark if age_shark != -1 else -10
+        self.energy_initial = energy_initial if energy_initial != -1 else 5
+        self.energy_eat = energy_eat if energy_eat != -1 else 3
 
         # WaTor planet setup
         error_msg = 'Either provide creatures or shape, nfish and nsharks'
         if creatures is not None:   # creatures provided
-            if nfish is not None or nsharks is not None or shape is not None:
+            if nfish != -1 or nsharks != -1 or shape is not None:
                 raise ValueError(error_msg)
             self.creatures = creatures.astype(numpy.int8, copy=False)
         else:  # create creatures
-            if shape is None or nfish is None or nsharks is None:
+            if shape is None or nfish == -1 or nsharks == -1:
                 raise ValueError(error_msg)
-            self.creatures = self._random_creatures(shape, nfish, nsharks)
-
-        if energies is not None:
-            if energies.shape[0] != self.creatures.shape[0] or energies.shape[1] != self.creatures.shape[1]:
-                raise ValueError('Shapes of creatures and energies must be '
-                                 'the same.')
-            if energy_initial is not None:
-                raise ValueError('Do not provide energy_initial together with '
-                                 'energies.')
-            self.energies = energies.astype(numpy.int16, copy=False)
-        else:
-            self.energies = numpy.zeros_like(self.creatures, dtype=numpy.int16)
-            self.energies[self.creatures < 0] = self.energy_initial
+            self.creatures = self._random_creatures(shape[0], shape[1], nfish, nsharks)
 
         self.height = self.creatures.shape[0]
         self.width = self.creatures.shape[1]
 
-    def count_fish(self):
+        if energies is not None:
+            if energies.shape[0] != self.creatures.shape[0] or energies.shape[1] != self.creatures.shape[1]:
+                raise ValueError('Shapes of creatures and energies must be the same.')
+            if energy_initial != -1:
+                raise ValueError('Do not provide energy_initial together with energies.')
+            self.energies = energies.astype(numpy.int16, copy=False)
+        else:
+            self.energies = numpy.zeros((self.height, self.width), dtype=numpy.int16)
+            self.energies[self.creatures < 0] = self.energy_initial
+
+    cpdef int count_fish(self):
         """Return number of fish."""
         # fish are represented as positive numbers
-        return numpy.sum(self.creatures > 0)
+        return (self.creatures > 0).sum()
 
-    def count_sharks(self):
+    cpdef int count_sharks(self):
         """Return number of sharks."""
         # fish are represented as negative numbers
-        return numpy.sum(self.creatures < 0)
+        return (self.creatures < 0).sum()
 
-    def _generate_move(self, x, y):
-        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
-        for i, j in numpy.random.permutation(directions):
-            a, b = (x + i) % self.height, (y + j) % self.width
-            if (a, b) != (x, y):  # cannot move to same field
-                yield a, b
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    cdef numpy.ndarray[numpy.int16_t, ndim=2] _generate_move(self, numpy.int16_t x, numpy.int16_t y):
+        cdef int idx
+        cdef numpy.ndarray[numpy.int16_t, ndim=2] directions = numpy.array([[-1, 0], [0, -1], [1, 0], [0, 1]], numpy.int16)
+        cdef numpy.ndarray[numpy.int16_t, ndim=2] moves = numpy.random.permutation(directions)
+        for idx in range(4):
+            moves[idx, 0] += x
+            moves[idx, 1] += y
+            moves[idx, 0] = moves[idx, 0] % self.height
+            moves[idx, 1] = moves[idx, 1] % self.width
+        return moves
 
     def _move_fish(self, new_creatures):
         for x, y in numpy.argwhere(self.creatures > 0):  # for each fish
@@ -96,8 +95,7 @@ cdef class WaTor:
                     break
 
             if not moved:
-                new_creatures[x, y] = min(self.creatures[x, y] + 1,
-                                          self.age_fish)
+                new_creatures[x, y] = min(self.creatures[x, y] + 1, self.age_fish)
 
         return new_creatures
 
@@ -120,8 +118,7 @@ cdef class WaTor:
             # find fish
             for a, b in self._generate_move(x, y):
                 if new_creatures[a, b] > 0:  # if there is fish eat it
-                    self._move_shark(new_creatures, new_energies, (x, y),
-                                     (a, b), self.energy_eat)
+                    self._move_shark(new_creatures, new_energies, (x, y), (a, b), self.energy_eat)
                     moved = True
                     break
 
@@ -131,14 +128,12 @@ cdef class WaTor:
             # move
             for a, b in self._generate_move(x, y):
                 if new_creatures[a, b] == 0:
-                    self._move_shark(new_creatures, new_energies,
-                                     (x, y), (a, b))
+                    self._move_shark(new_creatures, new_energies, (x, y), (a, b))
                     moved = True
                     break
 
             if not moved:
-                new_creatures[x, y] = max(self.creatures[x, y] - 1,
-                                          self.age_shark)
+                new_creatures[x, y] = max(self.creatures[x, y] - 1, self.age_shark)
                 new_energies[x, y] = self.energies[x, y]
 
         new_creatures[new_energies == 1] = 0
@@ -148,9 +143,8 @@ cdef class WaTor:
 
     def tick(self):
         """Simulate one chronon."""
-        new_creatures = numpy.copy(self.creatures)
-        new_energies = numpy.copy(self.energies)
-        new_fish = self._move_fish(new_creatures)
-        self.creatures, self.energies = self._move_sharks(new_fish,
-                                                          new_energies)
+        cdef numpy.ndarray[numpy.int8_t, ndim=2] new_creatures = numpy.copy(self.creatures)
+        cdef numpy.ndarray[numpy.int16_t, ndim=2] new_energies = numpy.copy(self.energies)
+        cdef numpy.ndarray[numpy.int8_t, ndim=2] new_fish = self._move_fish(new_creatures)
+        self.creatures, self.energies = self._move_sharks(new_fish, new_energies)
         return self
