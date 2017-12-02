dda7aa5054504bc91a4ea727599f814c1d287f45
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2017-12-02 14:07:24 +0100

    Copy cythonized impelmentation

--- wator/cwator.pyx
+++ wator/cwator.pyx
@@ -1,3 +1,6 @@
+# cythonize code by hroncok
+# available at https://github.com/hroncok/wator/blob/master/wator/_cwator.pyx
+
 #cython language_level=3
 #cython cdivision=3
 #cython boundscheck=False
@@ -43,3 +46,210 @@ def random_creatures(shape, int nfish, int nsharks,
                 c[i, j] = randint(age_shark, 0)
                 nsharks -= 1
     return c
+
+
+cdef bint is_fish(numpy.ndarray[I8, ndim=2] creatures, int i, int j):
+    return creatures[i, j] > 0
+
+
+cdef bint is_shark(numpy.ndarray[I8, ndim=2] creatures, int i, int j):
+    return creatures[i, j] < 0
+
+
+cdef bint is_empty(numpy.ndarray[I8, ndim=2] creatures, int i, int j):
+    return creatures[i, j] == 0
+
+
+cdef bint is_dead(numpy.ndarray[I64, ndim=2] energies, int i, int j):
+    return energies[i, j] <= 0
+
+
+cdef bint is_moved(numpy.ndarray[I8, ndim=2] moved, int i, int j):
+    return moved[i, j]
+
+
+cdef void move_one_fish(numpy.ndarray[I8, ndim=2] creatures,
+        numpy.ndarray[I64, ndim=2] energies, numpy.ndarray[I8, ndim=2] moved,
+        numpy.ndarray[I64, ndim=2] targets, int age_fish, int height,
+        int width, int i, int j):
+    cdef int count = 0
+    cdef int tmp, t0, t1
+
+    tmp = (i - 1 + height) % height
+    if is_empty(creatures, tmp, j):
+        targets[count, 0] = tmp
+        targets[count, 1] = j
+        count +=1
+
+    tmp = (i + 1) % height
+    if is_empty(creatures, tmp, j):
+        targets[count, 0] = tmp
+        targets[count, 1] = j
+        count +=1
+
+    tmp = (j - 1 + width) % width
+    if is_empty(creatures, i, tmp):
+        targets[count, 0] = i
+        targets[count, 1] = tmp
+        count +=1
+
+    tmp = (j + 1) % width
+    if is_empty(creatures, i, tmp):
+        targets[count, 0] = i
+        targets[count, 1] = tmp
+        count +=1
+
+    if count > 0:
+        tmp = randint(0, count)
+        t0 = targets[tmp, 0]
+        t1 = targets[tmp, 1]
+
+        if creatures[i, j] > age_fish:
+            creatures[t0, t1] = 1
+            creatures[i, j] = 1
+            moved[i, j] = True
+        else:
+            creatures[t0, t1] = creatures[i, j] + 1
+            creatures[i, j] = 0
+        moved[t0, t1] = True
+    else:
+        moved[i, j] = True
+        if creatures[i, j] <= age_fish:
+            creatures[i, j] += 1
+
+
+cdef void move_one_shark(numpy.ndarray[I8, ndim=2] creatures,
+        numpy.ndarray[I64, ndim=2] energies, numpy.ndarray[I8, ndim=2] moved,
+        numpy.ndarray[I64, ndim=2] targets, int age_shark, int energy_eat,
+        int height, int width, int i, int j):
+    cdef int count = 0
+    cdef int tmp, t0, t1
+
+    tmp = (i - 1 + height) % height
+    if is_fish(creatures, tmp, j):
+        targets[count, 0] = tmp
+        targets[count, 1] = j
+        count +=1
+
+    tmp = (i + 1) % height
+    if is_fish(creatures, tmp, j):
+        targets[count, 0] = tmp
+        targets[count, 1] = j
+        count +=1
+
+    tmp = (j - 1 + width) % width
+    if is_fish(creatures, i, tmp):
+        targets[count, 0] = i
+        targets[count, 1] = tmp
+        count +=1
+
+    tmp = (j + 1) % width
+    if is_fish(creatures, i, tmp):
+        targets[count, 0] = i
+        targets[count, 1] = tmp
+        count +=1
+
+    if count == 0:
+        tmp = (i - 1 + height) % height
+        if is_empty(creatures, tmp, j):
+            targets[count, 0] = tmp
+            targets[count, 1] = j
+            count +=1
+
+        tmp = (i + 1) % height
+        if is_empty(creatures, tmp, j):
+            targets[count, 0] = tmp
+            targets[count, 1] = j
+            count +=1
+
+        tmp = (j - 1 + width) % width
+        if is_empty(creatures, i, tmp):
+            targets[count, 0] = i
+            targets[count, 1] = tmp
+            count +=1
+
+        tmp = (j + 1) % width
+        if is_empty(creatures, i, tmp):
+            targets[count, 0] = i
+            targets[count, 1] = tmp
+            count +=1
+
+    energies[i, j] -= 1
+
+    if count > 0:
+        tmp = randint(0, count)
+        t0 = targets[tmp, 0]
+        t1 = targets[tmp, 1]
+
+        was_fish = is_fish(creatures, t0, t1)
+
+        if creatures[i, j] < age_shark:
+            creatures[t0, t1] = -1
+            creatures[i, j] = -1
+            moved[i, j] = True
+        else:
+            creatures[t0, t1] = creatures[i, j] - 1
+            creatures[i, j] = 0
+        moved[t0, t1] = True
+
+        energies[t0, t1] = energies[i, j]
+        if was_fish:
+            energies[t0, t1] += energy_eat
+    else:
+        moved[i, j] = True
+        if creatures[i, j] >= age_shark:
+            creatures[i, j] -= 1
+
+
+cdef void move_fish(numpy.ndarray[I8, ndim=2] creatures,
+        numpy.ndarray[I64, ndim=2] energies, numpy.ndarray[I8, ndim=2] moved,
+        numpy.ndarray[I64, ndim=2] targets, int age_fish, int height,
+        int width):
+    cdef int i, j
+    for i in range(height):
+        for j in range(width):
+            if is_fish(creatures, i, j) and not is_moved(moved, i, j):
+                move_one_fish(creatures, energies, moved, targets, age_fish,
+                              height, width, i, j)
+
+
+cdef void move_sharks(numpy.ndarray[I8, ndim=2] creatures,
+        numpy.ndarray[I64, ndim=2] energies, numpy.ndarray[I8, ndim=2] moved,
+        numpy.ndarray[I64, ndim=2] targets, int age_shark, int energy_eat,
+        int height, int width):
+    cdef int i, j
+    for i in range(height):
+        for j in range(width):
+            if is_shark(creatures, i, j) and not is_moved(moved, i, j):
+                move_one_shark(creatures, energies, moved, targets, age_shark,
+                        energy_eat, height, width, i, j)
+
+
+cdef void remove_dead_sharks(numpy.ndarray[I8, ndim=2] creatures,
+        numpy.ndarray[I64, ndim=2] energies, int height, int width):
+    cdef int i, j
+    for i in range(height):
+        for j in range(width):
+            if is_shark(creatures, i, j) and is_dead(energies, i, j):
+                creatures[i, j] = 0
+
+
+def tick(numpy.ndarray[I8, ndim=2] creatures,
+        numpy.ndarray[I64, ndim=2] energies, int age_fish, int age_shark,
+        int energy_eat):
+    cdef numpy.ndarray[I8, ndim=2] moved
+    cdef numpy.ndarray[I64, ndim=2] targets = \
+        numpy.ndarray((4, 2), dtype=numpy.int64)
+
+    cdef int height = creatures.shape[0]
+    cdef int width = creatures.shape[1]
+
+    moved = numpy.zeros((height, width), dtype=numpy.int8)
+    move_fish(creatures, energies, moved, targets, age_fish, height, width)
+
+    moved[::] = 0
+    move_sharks(creatures, energies, moved, targets, age_shark, energy_eat,
+                height, width)
+
+    remove_dead_sharks(creatures, energies, height, width)
+    return creatures, energies
--- wator/wator.py
+++ wator/wator.py
@@ -24,7 +24,8 @@ class WaTor:
             if shape is None or nfish is None or nsharks is None:
                 raise ValueError(error_msg)
             self.creatures = cwator.random_creatures(shape, nfish, nsharks,
-                    self.age_fish, self.age_shark)
+                                                     self.age_fish,
+                                                     self.age_shark)
 
         if energies is not None:
             if energies.shape != self.creatures.shape:
@@ -36,7 +37,7 @@ class WaTor:
             self.energies = energies.astype(numpy.int64, copy=False)
         else:
             self.energies = numpy.full(self.creatures.shape,
-                    self.energy_initial, dtype=numpy.int64)
+                                       self.energy_initial, dtype=numpy.int64)
 
         self.height, self.width = self.creatures.shape
 
@@ -50,83 +51,11 @@ class WaTor:
         # fish are represented as negative numbers
         return numpy.sum(self.creatures < 0)
 
-    def _generate_move(self, x, y):
-        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
-        for i, j in numpy.random.permutation(directions):
-            a, b = (x + i) % self.height, (y + j) % self.width
-            if (a, b) != (x, y):  # cannot move to same field
-                yield a, b
-
-    def _move_fish(self, new_creatures):
-        for x, y in numpy.argwhere(self.creatures > 0):  # for each fish
-            moved = False
-            for a, b in self._generate_move(x, y):
-                if new_creatures[a, b] == 0:
-                    if new_creatures[x, y] + 1 > self.age_fish:
-                        # reproduce
-                        new_creatures[x, y] = 1
-                        new_creatures[a, b] = 1
-                    else:  # move
-                        new_creatures[a, b] = self.creatures[x, y] + 1
-                        new_creatures[x, y] = 0
-                    moved = True
-                    break
-
-            if not moved:
-                new_creatures[x, y] = min(self.creatures[x, y] + 1,
-                                          self.age_fish)
-
-        return new_creatures
-
-    def _move_shark(self, new_creatures, new_energies, fr, to, energy_gain=0):
-        x, y = fr
-        a, b = to
-        if new_creatures[x, y] - 1 < self.age_shark:
-            new_creatures[x, y] = -1
-            new_energies[x, y] = self.energies[x, y]
-            new_creatures[a, b] = -1
-        else:
-            new_creatures[a, b] = self.creatures[x, y] - 1
-            new_creatures[x, y] = 0
-        new_energies[a, b] = self.energies[x, y] + energy_gain
-
-    def _move_sharks(self, new_creatures, new_energies):
-        for x, y in numpy.argwhere(self.creatures < 0):  # for each shark
-            moved = False
-            # find fish
-            for a, b in self._generate_move(x, y):
-                if new_creatures[a, b] > 0:  # if there is fish eat it
-                    self._move_shark(new_creatures, new_energies, (x, y),
-                                     (a, b), self.energy_eat)
-                    moved = True
-                    break
-
-            if moved:
-                continue
-
-            # move
-            for a, b in self._generate_move(x, y):
-                if new_creatures[a, b] == 0:
-                    self._move_shark(new_creatures, new_energies,
-                                     (x, y), (a, b))
-                    moved = True
-                    break
-
-            if not moved:
-                new_creatures[x, y] = max(self.creatures[x, y] - 1,
-                                          self.age_shark)
-                new_energies[x, y] = self.energies[x, y]
-
-        new_energies -= 1
-        new_creatures[(new_energies == 0) & (new_creatures < 0)] = 0
-
-        return new_creatures, new_energies
-
     def tick(self):
         """Simulate one chronon."""
-        new_creatures = numpy.copy(self.creatures)
-        new_energies = numpy.copy(self.energies)
-        new_fish = self._move_fish(new_creatures)
-        self.creatures, self.energies = self._move_sharks(new_fish,
-                                                          new_energies)
+        self.creatures, self.energies = cwator.tick(self.creatures,
+                                                    self.energies,
+                                                    self.age_fish,
+                                                    self.age_shark,
+                                                    self.energy_eat)
         return self
